f54c40adf6959c005c330741b31900f8cb9b0711
pankaj <pankajchopra619@gmail.com>
2018-01-09 15:15:29 +0530

    Notices and Notifications Fragment added! Minor changes to other files also.

--- app/src/main/java/com/example/hp/campusconnect/AddComplaintFragment.java
+++ app/src/main/java/com/example/hp/campusconnect/AddComplaintFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Fragment_add.OnFragmentInteractionListener} interface
+ * {@link AddComplaintFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Fragment_add#newInstance} factory method to
+ * Use the {@link AddComplaintFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Fragment_add extends Fragment {
+public class AddComplaintFragment extends android.app.Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class Fragment_add extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Fragment_add() {
+    public AddComplaintFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class Fragment_add extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_add.
+     * @return A new instance of fragment AddComplaintFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Fragment_add newInstance(String param1, String param2) {
-        Fragment_add fragment = new Fragment_add();
+    public static AddComplaintFragment newInstance(String param1, String param2) {
+        AddComplaintFragment fragment = new AddComplaintFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class Fragment_add extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_add, container, false);
+        return inflater.inflate(R.layout.fragment_add_complaint, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/hp/campusconnect/MainActivity.java
+++ app/src/main/java/com/example/hp/campusconnect/MainActivity.java
@@ -1,21 +1,23 @@
 package com.example.hp.campusconnect;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button btn;
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.button_main);
+        btn = findViewById(R.id.button_main);
     }
-    public void onc(View v){
-        Intent i=new Intent(this,Home.class);
+
+    public void onc(View v) {
+        Intent i = new Intent(this, HomeActivity.class);
         startActivity(i);
     }
 
